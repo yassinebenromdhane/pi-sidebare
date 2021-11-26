4d9bb61ea5326a75a5aaac7cfdc99741b1bcc66b
med yassine ben romdhane <yassine.beno@outlook.fr>
2021-11-26 22:38:52 +0000

    ok

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>
+            <div className="height-100 bg-light">
+            <h4>Main Components</h4>
+        </div>
+        </div>
+    )
+}
+
+export default Main
